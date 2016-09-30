3f16858e2234f208a03ac9e8bfdeee013e39be25
prashant143 <prashant.vaghela@mygeorgian.ca>
2016-09-30 10:47:42 -0400

    Updated user file and deleted bootstrap file

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
 
   //load a users view instead
-  var userList=['Harsh','Mahesh','Baba Ramdev'];
+  var userList=['Harsh','Mahesh','Baba Ramdev','Bhana Bhai'];
   res.render('users', {title: 'User List',
             users: userList});
 });
